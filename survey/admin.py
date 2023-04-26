719f82155092a8cf24706bb1a38a83fd5ac9f7f8
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2023-04-26 15:51:18 +0800

    software section lates UI and list of questions

--- survey/admin.py
+++ survey/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Survey, office_division
+from .models import Survey, office_division, ProfessionalToolsOption, CompetencyScale, ICTTrainingPrograms, CommunicationToolsOption, ProductivityOption
 # Register your models here.
 
 admin.site.register(Survey)
 admin.site.register(office_division)
+admin.site.register(ProfessionalToolsOption)
+admin.site.register(CompetencyScale)
+admin.site.register(ICTTrainingPrograms)
+admin.site.register(CommunicationToolsOption)
+admin.site.register(ProductivityOption)
 
